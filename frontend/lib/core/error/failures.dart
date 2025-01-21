88f438b738d480197b44fc6ba1c31644e7aa519e
yohanse <mehabawyohanse793@gmail.com>
2025-01-21 20:52:40 +0300

    create failures

---
+++ frontend/lib/core/error/failures.dart
@@ -0,0 +1,62 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  final String message;
+  const Failure(this.message, [List props = const <dynamic>[]]) : super();
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure(super.message);
+  @override
+  List<Object?> get props => [];
+}
+
+class CacheFailure extends Failure {
+  const CacheFailure(super.message);
+  @override
+  List<Object?> get props => [];
+}
+
+class NetworkFailure extends Failure {
+  const NetworkFailure(super.message);
+  @override
+  List<Object?> get props => [];
+}
+
+class EmailNotCorrectFailure extends Failure {
+  const EmailNotCorrectFailure(super.message);
+  @override
+  List<Object?> get props => [];
+}
+
+class EmailDoesNotExistFaliure extends Failure {
+  const EmailDoesNotExistFaliure(super.message);
+  @override
+  List<Object?> get props => [];
+}
+
+class EmailAlreadyExistFaliure extends Failure {
+  const EmailAlreadyExistFaliure(super.message);
+  @override
+  List<Object?> get props => [];
+}
+
+class NoTokenFailure extends Failure {
+  const NoTokenFailure(super.message);
+  @override
+  List<Object?> get props => [];
+}
+
+class UsernameAndPasswordDoesNotMatchFailure extends Failure{
+  const UsernameAndPasswordDoesNotMatchFailure(super.message);
+  @override
+  List<Object?> get props => [];
+}
+
+
+
+class WrongEmailFormatFailure extends Failure{
+  const WrongEmailFormatFailure(super.message);
+  @override
+  List<Object?> get props => [];
+}
