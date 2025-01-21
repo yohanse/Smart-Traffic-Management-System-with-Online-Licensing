ea63fb0bab687d3e77ded5b757900b58a65dfee0
yohanse <mehabawyohanse793@gmail.com>
2025-01-21 20:52:29 +0300

    create exceptions

---
+++ frontend/lib/core/error/exception.dart
@@ -0,0 +1,17 @@
+class ServerException implements Exception {}
+
+class CacheException implements Exception {}
+
+class EmailNotCorrectException implements Exception {}
+
+class EmailAlreadyExistException implements Exception {}
+
+class EmailDoesNotExistException implements Exception {}
+
+class UsernameAlreadyExistException implements Exception {}
+
+class NoTokenException implements Exception {}
+
+class UsernameAndPasswordDoesNotMatchException implements Exception {}
+
+class WrongEmailFormatException implements Exception {}
