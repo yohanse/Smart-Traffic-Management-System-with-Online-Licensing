ca50541acc85573cb59742d0c74faeaeebb880f3
yohanse <mehabawyohanse793@gmail.com>
2025-01-21 20:55:29 +0300

    create a local datasource to manage tokens

---
+++ frontend/lib/features/auth/data/datasources/local.dart
@@ -0,0 +1,32 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class AuthLocalDataSource {
+  SharedPreferences get prefs;
+
+  void cacheToken({required String accessToken, required String refreshToken});
+  void deleteToken();
+  String? getToken();
+}
+
+class AuthLocalDataSourceImpl implements AuthLocalDataSource {
+  final SharedPreferences prefs;
+
+  AuthLocalDataSourceImpl({required this.prefs});
+
+  @override
+  cacheToken({required String accessToken, required String refreshToken}) async {
+    await prefs.setString('accessToken', accessToken);
+    await prefs.setString('refreshToken', refreshToken);
+  }
+
+  @override
+  deleteToken() async {
+    await prefs.remove('accessToken');
+    await prefs.remove('refreshToken');
+  }
+
+  @override
+  String? getToken() {
+    return prefs.getString('accessToken');
+  }
+}
