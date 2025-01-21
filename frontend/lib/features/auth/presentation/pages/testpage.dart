595677d758b0d1bf3010611e71807c09cd445aed
yohanse <mehabawyohanse793@gmail.com>
2025-01-21 20:51:13 +0300

    create test page to check logged in successfully

---
+++ frontend/lib/features/auth/presentation/pages/testpage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TestPage extends StatelessWidget {
+  const TestPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Logged in successfully !!!"),
+      ),
+    );
+  }
+}
