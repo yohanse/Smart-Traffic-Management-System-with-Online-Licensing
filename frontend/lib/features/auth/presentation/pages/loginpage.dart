3302e615bef38d4074fbaa3fabf22955ebca49aa
yohanse <mehabawyohanse793@gmail.com>
2025-01-21 20:56:38 +0300

    add bloc consumer of login bloc

--- frontend/lib/features/auth/presentation/pages/loginpage.dart
+++ frontend/lib/features/auth/presentation/pages/loginpage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/features/auth/presentation/bloc/bloc/login_bloc.dart';
 import 'package:frontend/features/auth/presentation/widgets/continue_with_google_button.dart';
 import 'package:frontend/features/auth/presentation/widgets/custom_email_field.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../utility/or_line.dart';
 import '../widgets/custom_password_field.dart';
@@ -12,55 +15,76 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            flex: 4,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.arrow_back,
+    return BlocConsumer<LoginBloc, LoginState>(
+      listener: (context, state) {
+        if (state is LoginSuccess) {
+          context.go("/test");
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          body: Column(
+            children: [
+              Expanded(
+                flex: 4,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    IconButton(
+                      onPressed: () => context.go("/"),
+                      icon: Icon(
+                        Icons.arrow_back,
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Login in to your \nAccount',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 6,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    spacing: 14,
+                    children: [
+                      CustomEmailField(controller: emailController),
+                      CustomPasswordField(
+                        passwordController: passwordController,
+                      ),
+                      ElevatedButton(
+                        onPressed: state is LoginLoading
+                            ? null
+                            : () {
+                                print("Click 1");
+                                context.read<LoginBloc>().add(
+                                      LoginStarted(
+                                        email: emailController.text,
+                                        password: passwordController.text,
+                                      ),
+                                    );
+                              },
+                        child: state is LoginLoading
+                            ? CircularProgressIndicator()
+                            : Text('Login'),
+                      ),
+                      OrLine(),
+                      ContinueWithGoogleButton(),
+                    ],
                   ),
                 ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Login in to your \nAccount',
-                    style: Theme.of(context).textTheme.bodyLarge,
-                  ),
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 6,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                spacing: 14,
-                children: [
-                  CustomEmailField(controller: emailController),
-                  CustomPasswordField(
-                    passwordController: passwordController,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Sign in'),
-                  ),
-                  OrLine(),
-                  ContinueWithGoogleButton(),
-                ],
               ),
-            ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
