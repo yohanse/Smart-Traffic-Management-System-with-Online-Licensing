ca7fc37b11f8a163f8a4db871fde7734253e7de9
yohanse <mehabawyohanse793@gmail.com>
2025-01-22 17:32:14 +0300

    Add sign in with google to repository

--- frontend/lib/features/auth/data/repositories/auth_repository_impl.dart
+++ frontend/lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -4,15 +4,19 @@ import 'package:frontend/core/error/failures.dart';
 
 import '../../../../core/error/exception.dart';
 import '../../../../core/network/network_info.dart';
+import '../../../../core/services/google_login_service.dart';
 import '../../domain/repositories/auth_repository.dart';
 import '../datasources/remote.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
   final AuthRemoteDataSource authRemoteDataSource;
   final NetworkInfo networkInfo;
+  final GoogleSignInService googleSignInService;
 
   AuthRepositoryImpl(
-      {required this.authRemoteDataSource, required this.networkInfo});
+      {required this.authRemoteDataSource,
+      required this.networkInfo,
+      required this.googleSignInService});
 
   @override
   Future<Either<Failure, bool>> logIn(String email, String password) async {
@@ -24,9 +28,26 @@ class AuthRepositoryImpl implements AuthRepository {
         return const Left(ServerFailure("Server not working properly."));
       } on EmailNotCorrectException {
         return const Left(EmailNotCorrectFailure("Email not correct"));
+      } catch (e) {
+
+        return const Left(ServerFailure("Server not working properly."));
       }
     } else {
       return const Left(NetworkFailure("Netwrok error."));
     }
+  }
+
+  @override
+  Future<Either<Failure, bool>> signInWithGoogle() async {
+    final idToken = await googleSignInService.signInWithGoogle();
+    if (idToken != null) {
+      try {
+        await authRemoteDataSource.verifyGoogleToken(idToken);
+        return Right(true);
+      } catch (e) {
+        return Left(ServerFailure("Server not working properly."));
+      }
+    }
+    return Left(ServerFailure("Server not working properly."));
   }
 }
--- frontend/lib/features/auth/domain/repositories/auth_repository.dart
+++ frontend/lib/features/auth/domain/repositories/auth_repository.dart
@@ -3,4 +3,5 @@ import '../../../../core/error/failures.dart';
 
 abstract class AuthRepository {
   Future<Either<Failure, bool>> logIn(String email, String password);
+  Future<Either<Failure, bool>>signInWithGoogle();
 }
