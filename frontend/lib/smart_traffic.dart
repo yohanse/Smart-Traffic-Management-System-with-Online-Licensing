1b82bc165f87d0df0e7dbbaa09bd9e0aad4041b3
yohanse <mehabawyohanse793@gmail.com>
2025-01-22 17:36:01 +0300

    provide signinwithgooglebloc

--- frontend/lib/smart_traffic.dart
+++ frontend/lib/smart_traffic.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/features/auth/presentation/bloc/signin/signinwithgoogle_bloc.dart';
 import 'package:frontend/features/auth/presentation/pages/homepage.dart';
 import 'package:frontend/features/auth/presentation/pages/loginpage.dart';
 import 'package:frontend/features/auth/presentation/pages/testpage.dart';
@@ -7,14 +8,14 @@ import 'package:frontend/injection.dart';
 import 'package:frontend/utility/theme.dart';
 import 'package:go_router/go_router.dart';
 
-import 'features/auth/presentation/bloc/bloc/login_bloc.dart';
+import 'features/auth/presentation/bloc/login/login_bloc.dart';
 
 class SmartTrafficApp extends StatelessWidget {
   const SmartTrafficApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final GoRouter _router = GoRouter(
+    final GoRouter router = GoRouter(
       routes: [
         GoRoute(
           path: "/",
@@ -35,13 +36,16 @@ class SmartTrafficApp extends StatelessWidget {
         BlocProvider<LoginBloc>(
           create: (context) => sl<LoginBloc>(),
         ),
+        BlocProvider<SigninwithgoogleBloc>(
+          create: (context) => sl<SigninwithgoogleBloc>(),
+        ),
       ],
       child: MaterialApp.router(
         theme: lightTheme,
         darkTheme: darkTheme,
         themeMode: ThemeMode.system,
         debugShowCheckedModeBanner: false,
-        routerConfig: _router,
+        routerConfig: router,
       ),
     );
   }
