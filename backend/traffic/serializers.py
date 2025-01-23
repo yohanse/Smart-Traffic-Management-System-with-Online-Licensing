7c4de7c5a133bb2602ea7c805dcee0e4fb67e85c
yohanse <mehabawyohanse793@gmail.com>
2025-01-23 14:19:09 +0300

    Create: serializer for ViolationType

--- backend/traffic/serializers.py
+++ backend/traffic/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework.serializers import Serializer, ModelSerializer
-from .models import Driver, Violation
+from .models import Driver, ViolationType
 
 class DriverSerializer(ModelSerializer):
     class Meta:
         model = Driver
         fields = ['id', 'license_number', 'first_name', 'last_name', 'date_of_birth', 'address', 'sex', 'region', 'woreda', 'house_number', 'phone_number', 'nationality', 'license_issue_date', 'blood_type', 'profile_image']
+
+
+class ViolationTypeSerializer(ModelSerializer):
+    class Meta:
+        model = ViolationType
+        fields = ['id', 'name', 'description', 'fine_amount']
+        
