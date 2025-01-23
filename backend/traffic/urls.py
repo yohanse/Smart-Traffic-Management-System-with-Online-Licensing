5701ebb717c0c8ebcbcac18c631903df29fcd22d
yohanse <mehabawyohanse793@gmail.com>
2025-01-23 14:20:55 +0300

    Create: a router for violation-type view

--- backend/traffic/urls.py
+++ backend/traffic/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import DriverView
+from .views import DriverView, ViolationTypeView
 
 router = DefaultRouter()
 
 router.register('drivers', DriverView, basename='driver')
+router.register('violation-types', ViolationTypeView, basename='violation-type')
