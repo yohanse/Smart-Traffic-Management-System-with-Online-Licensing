85a557718c8dcc4cc21a374afa133ca72a5338a1
yohanse <mehabawyohanse793@gmail.com>
2025-01-23 14:09:23 +0300

    Create: serializer for driver model

---
+++ backend/traffic/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import Serializer, ModelSerializer
+from .models import Driver, Violation
+
+class DriverSerializer(ModelSerializer):
+    class Meta:
+        model = Driver
+        fields = ['id', 'license_number', 'first_name', 'last_name', 'date_of_birth', 'address', 'sex', 'region', 'woreda', 'house_number', 'phone_number', 'nationality', 'license_issue_date', 'blood_type', 'profile_image']
