418d9da2b03c3046a9d9181d01bd87724a2e1b09
yohanse <mehabawyohanse793@gmail.com>
2025-01-23 14:12:27 +0300

    Create: view for driver model

--- backend/traffic/views.py
+++ backend/traffic/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from .models import Driver
+from .serializers import DriverSerializer
 
+
+class DriverView(ModelViewSet):
+    queryset = Driver.objects.all()
+    serializer_class = DriverSerializer
+    
 # Create your views here.
