5801febb43107c25282dd3d98bb8b562ea3bae03
yohanse <mehabawyohanse793@gmail.com>
2025-01-23 14:20:01 +0300

    Create: a view for violation type

--- backend/traffic/views.py
+++ backend/traffic/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
-from .models import Driver
-from .serializers import DriverSerializer
+from .models import Driver, ViolationType
+from .serializers import DriverSerializer, ViolationTypeSerializer
 
 
 class DriverView(ModelViewSet):
     queryset = Driver.objects.all()
     serializer_class = DriverSerializer
-    
-# Create your views here.
+
+
+
+class ViolationTypeView(ModelViewSet):
+    queryset = ViolationType.objects.all()
+    serializer_class = ViolationTypeSerializer
